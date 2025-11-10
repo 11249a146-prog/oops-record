43a615ea749e1011f9abe2185f49449ef5b01294
11249a146-prog <11249a146@kanchiuniv.ac.in>
2025-11-10 18:25:45 +0530

    Add unary operator overloading for temperature conversion
    Implemented unary operator overloading to convert Celsius to Fahrenheit.

---
+++ Unary Operator Overloading.cpp
@@ -0,0 +1,28 @@
+              //Unary Operator Overloading//
+Aim:
+  //To overload the unary operator.//
+Algorithm:
+  1. Create Temperature class with celsius as data member.
+  2. Overload unary - to convert to Fahrenheit.
+  3. Display both values.
+Program:
+  #include <iostream>
+  using namespace std;
+  class Temperature {
+   float celsius;
+  public:
+   void getData() {
+   cout << "Enter temperature in Celsius: ";
+   cin >> celsius;
+   }
+   void operator-() {
+   float fahrenheit = (celsius * 9/5) + 32;
+   cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+   }
+  };
+  int main() {
+   Temperature t;
+   t.getData();
+   -t;
+   return 0;
+  }
